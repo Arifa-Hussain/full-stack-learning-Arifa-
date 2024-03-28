81e622a1a12b86fbaa8827eea5d4b371007bf92e
Arifa Hussain <batoolarifa259@gmail.com>
2024-03-28 16:06:43 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+
+function App() {
+ return(
+    <>
+      <h1>Arifa </h1>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe doloremque, sequi rerum fugiat atque facilis nesciunt tempore similique ipsam veniam corporis quo accusantium, eveniet illo magni possimus dicta! Dolore, odit.</p>
+    </>
+ )
+}
+
+export default App
